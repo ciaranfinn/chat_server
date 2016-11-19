5fb0aa49beac234ea699e4728da8b19a14f3cb72
Ciaran Finn <cifinn@tcd.ie>
2016-11-19 17:15:34 +0000

    broadcast feature implemented in elixir

--- lib/server/chat_room.ex
+++ lib/server/chat_room.ex
@@ -1,14 +1,14 @@
 defmodule Server.ChatRoom do
 
-  def join(room_name, join_id) do
-    Registry.register(__MODULE__, room_name, join_id)
+  def join(room_name, join_id,socket) do
+    Registry.register(__MODULE__, room_name, {join_id,socket})
   end
 
   def all_members(room_name) do
     Registry.lookup(__MODULE__, room_name)
   end
 
-  def broadcast(message, room_name, action) do
+  def broadcast(room_name, action) do
     Registry.dispatch(__MODULE__, room_name, action)
   end
 
--- lib/server/message.ex
+++ lib/server/message.ex
@@ -16,9 +16,7 @@ defmodule Server.Message do
   # ---------------- PROCESS MESSAGE TYPE -----------------------
 
   def process( socket, "HELO" <> " " <> text ) do
-    payload = "HELO #{text}IP:#{ip_address}\nPort:#{@port}\nStudentID:13320900\n"
-    :gen_tcp.send(socket,payload)
-    :gen_tcp.close(socket)
+    handle_hello_message(socket, String.strip(text))
   end
 
   def process(  _ , "KILL_SERVICE" <> _ ) do
@@ -27,18 +25,24 @@ defmodule Server.Message do
 
   def process(  socket , "JOIN_CHATROOM:" <> room_ref ) do
     handle_new_member(socket,String.strip(room_ref))
+    IO.inspect Server.ChatRoom.all_members(String.strip(room_ref))
   end
 
   def process(  _ , "DISCONNECT:" <> chatroom ) do
     IO.puts chatroom
   end
 
-  def process(  _ , "CHAT:" <> room_ref ) do
+  def process(  socket , "CHAT:" <> room_ref ) do
     IO.inspect Server.ChatRoom.all_members(String.strip(room_ref))
+    handle_chat_message(socket,String.strip(room_ref))
   end
 
   def process(  _ , "CLIENT_IP:" <> ip ) do
     String.strip(ip)
+  end
+
+  def process(  _ , "JOIN_ID:" <> join_id ) do
+    String.strip(join_id)
   end
 
   def process(  _ , "PORT:" <> port ) do
@@ -49,30 +53,68 @@ defmodule Server.Message do
     String.strip(name)
   end
 
+  def process(  _ , "MESSAGE:" <> message ) do
+    String.strip(message)
+  end
+
   def process(  _ , "LEAVE_CHATROOM:" <> room_ref ) do
     String.strip(room_ref)
     # handle_leave(socket, String.strip(room_ref))
   end
 
   def process( socket , _ ) do
-    payload = "ERROR_CODE:1\nERROR_DESCRIPTION:Unknown Message Format\n"
+    handle_error(socket)
+  end
+
+
+  # --------------------- HANDLERS -------------------------
+
+  defp handle_hello_message(socket,text) do
+    payload = "HELO #{text}IP:#{ip_address}\nPort:#{@port}\nStudentID:13320900\n"
     :gen_tcp.send(socket,payload)
     :gen_tcp.close(socket)
   end
 
   defp handle_new_member(socket,chatroom_name) do
-    read_line(socket)
-    read_line(socket)
-    read_line(socket)
+    read_line(socket) #ignore CLIENT_IP (TCP)
+    read_line(socket) #ignore PORT (TCP)
+    client_name = read_line(socket)
 
-    Server.ChatRoom.join(chatroom_name, 1)
+    Server.ChatRoom.join(chatroom_name, 1, socket)
     payload = "JOINED_CHATROOM: #{chatroom_name}\nSERVER_IP: #{ip_address}\nPORT: #{@port}\n"
     :gen_tcp.send(socket,payload)
   end
 
-  # defp handle_leave( socket, chatroom_name) do
-  #
-  # end
+  defp handle_error(socket) do
+    payload = "ERROR_CODE:1\nERROR_DESCRIPTION:Unknown Message Format\n"
+    :gen_tcp.send(socket,payload)
+    :gen_tcp.close(socket)
+  end
+
+  defp handle_chat_message(socket,room_ref) do
+    join_id = read_line(socket)
+    client_name = read_line(socket)
+    message = read_line(socket)
+
+
+    IO.puts join_id
+    IO.puts client_name
+    IO.puts message
+
+    notify_all(client_name, room_ref, message)
+  end
+
+
+
+  def notify_all(client_name, room_ref, data) do
+    payload = "CHAT: #{room_ref}\nCLIENT_NAME: #{client_name}\nMESSAGE: #{data}\n\n"
+    Server.ChatRoom.broadcast(room_ref, fn members ->
+      for { _, { _, socket} } <- members do
+        :gen_tcp.send(socket,payload)
+      end
+    end)
+
+  end
 
 
   # --------------------- GET LOCAL IP -----------------------
